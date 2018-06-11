078af266372620f7f4accd3d8d14b9e056abce76
JS <js@JSs-MacBook-Pro.local>
2018-06-11 13:40:12 -0600

    rearrange specs

--- spec/features/triple_crown_spec.rb
+++ spec/features/triple_crown_spec.rb
@@ -1,6 +1,9 @@
 describe "Triple Crown Winner" do
   it "returns a single player who won the crown stat or nil if no winner" do
     juicer = create(:juiced_player)
+    create(:hit_stat)
+    expect(Player.count).to eq(2)
+    expect(HitStat.count).to eq(3)
     expect(HitStat.triple_crown_winner(year: 2008, league: 'AL')).to eq(juicer)
   end
 
